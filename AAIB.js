f3af7cfdd45b86ea46cae6e20d8bb3b5945e159d
Matthew Martin <matthewmartin989@gmail.com>
2017-08-01 13:04:19 -0400

    Main Node stuff
    Made the main javascript file. AAIB = Aclevo AI Bot

---
+++ AAIB.js
@@ -0,0 +1,13 @@
+//=====IMPORTANT STUFF=======
+//Aclevo Bot AI
+//Author: Aclevo
+//Contributors: TheCodingGuy, 
+//License: MIT
+//Website: https://aclevo.xyz
+//Copyright Aclevo 2017-2018
+//===========================
+
+// CONFIGURE BOT
+const Discord = require("discord.js")
+const client = new Discord.Client();
+const config = require("./Settings.JSON")
